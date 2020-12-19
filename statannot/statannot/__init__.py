eb7785160bebcfe1078abc92dcf5903c23ad3866
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2020-12-19 10:15:27 +0000

    paper figures

---
+++ statannot/statannot/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['statannot']
+
+from .statannot import add_stat_annotation
+from .statannot import pval_annotation_text
+from .statannot import stat_test
+from ._version import __version__
